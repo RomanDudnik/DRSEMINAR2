8587a58ac8a6beb3527a2c85d03c6ce449a15520
«Roman <research777@mail.ru>
2022-11-17 17:15:01 +0300

    DRSEMINAR2_Ex13

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@
 //782 -> 8
 //918 -> 1
 
+/*
 int SecondNum2Digits (int number)
 {
     int current = (number % 100) / 10;
@@ -32,15 +33,35 @@ if ( userNum > 99 && userNum < 1000)
     Console.WriteLine ($"Second digit of the number {userNum} is {result} ");
 }
 else Console.WriteLine ($"Number {userNum} is not three digits! Enter a THREE-DIGIT number!");
-
+*/
 
 //Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
 //645 -> 5
-
 //78 -> третьей цифры нет
-
 //32679 -> 6
+
+/*
+int ThreeNumDigits (int number)
+{
+    while ( number > 1000)
+    {
+        number = number / 10;
+    }
+    int result = number % 10;
+    return result;
+}
+
+Console.WriteLine($"enter any number: ");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+if ( userNum > 99)
+{
+    int ThreeNum = ThreeNumDigits (userNum);
+    Console.WriteLine($"The third digit of your number {userNum} is {ThreeNum} ");
+}
+else Console.WriteLine($"The entered number {userNum} does not contain the third digit");
+*/
 
 //Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
